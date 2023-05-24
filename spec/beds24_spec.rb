d6d844eec9d3be2cc8a68752250bc35595545c6b
iuhoay <iuhoay@gmail.com>
2023-05-24 10:11:02 +0800

    Initial commit

---
+++ spec/beds24_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Beds24 do
+  it "has a version number" do
+    expect(Beds24::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
